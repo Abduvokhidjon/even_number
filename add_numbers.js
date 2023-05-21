c4a30f93cf51c1d5cbb3f180d88df1e2898ad539
Abduvokhidjon <aabduvohid33@gmail.com>
2023-05-20 19:09:14 -0500

    Task 4

---
+++ add_numbers.js
@@ -0,0 +1,10 @@
+/*
+Task 4: Add numbers using the + operator
+
+
+Console log the result of adding two numbers, 100 and 10, using the + operator.
+
+Note: This task should be completed on a single line of code. The output in the console should be 110.
+*/
+
+console.log(100 + 10)
